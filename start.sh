c41ade28b0fa18e1ce1cc51f129c5c9e59079271
ares2255 <aria.hurtada.ui@phinmaed.com>
2026-05-04 15:38:58 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 PORT="${PORT:-80}"
 
-# Set Apache to listen on Railway's PORT
 sed -i "s/Listen 80/Listen ${PORT}/" /etc/apache2/ports.conf
 sed -i "s/<VirtualHost \*:80>/<VirtualHost *:${PORT}>/" /etc/apache2/sites-enabled/000-default.conf
 
