b7c1f1e7b3702a833932487787bff81c89077c3a
ares2255 <aria.hurtada.ui@phinmaed.com>
2026-05-04 15:45:02 +0800

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-PORT="${PORT:-80}"
-
-sed -i "s/Listen 80/Listen ${PORT}/" /etc/apache2/ports.conf
-sed -i "s/<VirtualHost \*:80>/<VirtualHost *:${PORT}>/" /etc/apache2/sites-enabled/000-default.conf
-
-echo "Starting Apache on port ${PORT}..."
-exec apachectl -D FOREGROUND
